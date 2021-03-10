fcd178f2a4c5626fb9296fa692afc965cf804d40
Julia <juliyanevar@gmail.com>
2021-03-10 09:23:19 +0300

    lab3

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        public static double countInformationWithMistake(int N, double p)
+        {
+            double q = 1 - p;
+            double result = 0;
+            result = -p * Math.Log(p,2)- q * Math.Log(q,2);
+            return N * (1 - result);
+        }
+
+        public static double EntropySennon(string resultText, Regex regex)
+        {
+            double entropy = 0;
+            double v = 0;
+
+            double count = resultText.Count();
+            char[] ch = resultText.ToCharArray().Distinct().OrderBy(i => i).ToArray();
+
+            for (int i = 0; i < ch.Length; i++)
+            {
+                Console.WriteLine("Символ: {0}. Количество: {1}. Вероятность: {2}%.",
+                    ch[i], resultText.ToCharArray().Count(x => x == ch[i]), (resultText.ToCharArray().Count(x => x == ch[i]) / count) * 100);
+                entropy += (resultText.ToCharArray().Count(x => x == ch[i]) / count) * Math.Log((resultText.ToCharArray().Count(x => x == ch[i]) / count), 2);
+                v += resultText.ToCharArray().Count(x => x == ch[i]) / count;
+            }
+            Console.WriteLine("Общая вероятность: " + v);
+
+            return -entropy;
+        }
+
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Julia\Documents\учеба\6 сем\КМЗИ\labs\lab2\lab2\file0.txt";
+            string file = File.ReadAllText(path, Encoding.Default);
+            Regex regex = new Regex(@"[А-Яа-я]");
+            double ruEntr;
+            String resultText = "";
+            MatchCollection matches = regex.Matches(file);
+            foreach (Match match in matches)
+                resultText += match;
+            ruEntr = EntropySennon(resultText, regex);
+            Console.WriteLine("RU: энтропия: " + ruEntr.ToString());
+            Console.WriteLine();
+
+            regex = new Regex(@"[0-1]");
+            string binText = "";
+            String resultTextBin = "";
+            double ruBinEntr;
+            for (int i = 0; i < resultText.Length; i++)
+            {
+                binText += Convert.ToString((int)resultText[i], 2) + " ";
+            }
+            matches = regex.Matches(binText);
+            foreach (Match match in matches)
+                resultTextBin += match;
+            ruBinEntr = EntropySennon(resultTextBin, regex);
+            Console.WriteLine("RU BIN: энтропия: " + ruBinEntr.ToString()+"\n");
+            Console.WriteLine();
+
+            path = @"C:\Users\Julia\Documents\учеба\6 сем\КМЗИ\labs\lab2\lab2\file1.txt";
+            file = File.ReadAllText(path, Encoding.Default);
+            regex = new Regex(@"[A-Za-z]");
+            double enEntr;
+            resultText = "";
+            matches = regex.Matches(file);
+            foreach (Match match in matches)
+                resultText += match;
+            enEntr = EntropySennon(resultText, regex);
+            Console.WriteLine("EN: энтропия: " + enEntr.ToString());
+            Console.WriteLine();
+
+            regex = new Regex(@"[0-1]");
+            binText = "";
+            resultTextBin = "";
+            double enBinEntr;
+            for (int i = 0; i < resultText.Length; i++)
+            {
+                binText += Convert.ToString((int)resultText[i], 2) + " ";
+            }
+            matches = regex.Matches(binText);
+            foreach (Match match in matches)
+                resultTextBin += match;
+            enBinEntr = EntropySennon(resultTextBin, regex);
+            Console.WriteLine("EN BIN: энтропия: " + enBinEntr.ToString()+"\n");
+            Console.WriteLine();
+
+
+            string FIO = "Невар Юлия Валерьевна";
+            string FIOEn = "Nevar Julia Valeryevna";
+            String FIOBinRu = "";
+            for (int i = 0; i < FIO.Length; i++)
+            {
+                FIOBinRu += Convert.ToString((int)FIO[i], 2) + " ";
+            }
+            String FIOBinEn = "";
+            for (int i = 0; i < FIOEn.Length; i++)
+            {
+                FIOBinEn += Convert.ToString((int)FIOEn[i], 2) + " ";
+            }
+            Console.WriteLine("Количество информации RU:" + ruEntr*FIO.Length);
+            Console.WriteLine("Количество информации RU bin:" + ruBinEntr*FIOBinRu.Length);
+            byte[] bytesASCIIRu = Encoding.ASCII.GetBytes(FIO);
+            string ASCIIRu = "";
+            foreach (var b in bytesASCIIRu)
+                ASCIIRu += b;
+            Console.WriteLine("Количество информации RU ASCII:" + ruBinEntr* ASCIIRu.Length);
+            Console.WriteLine();
+
+
+            Console.WriteLine("Количество информации EN:" + enEntr*FIOEn.Length);
+            Console.WriteLine("Количество информации EN bin:" + enBinEntr* FIOBinEn.Length);
+            byte[] bytesASCIIEn = Encoding.ASCII.GetBytes(FIOEn);
+            string ASCIIEn = "";
+            foreach (var b in bytesASCIIEn)
+                ASCIIEn += b;
+            Console.WriteLine("Количество информации EN ASCII:" + enBinEntr*ASCIIEn.Length);
+            Console.WriteLine();
+
+
+            Console.WriteLine("RU Error 0,1: " + countInformationWithMistake(ASCIIRu.Length, 0.9));
+            Console.WriteLine("RU Error 0,5: " + countInformationWithMistake(ASCIIRu.Length, 0.5));
+            Console.WriteLine("RU Error 1: " + countInformationWithMistake(ASCIIRu.Length, 1));
+            Console.WriteLine("EN Error 0,1: " + countInformationWithMistake(ASCIIEn.Length, 0.9));
+            Console.WriteLine("EN Error 0,5: " + countInformationWithMistake(ASCIIEn.Length, 0.5));
+            Console.WriteLine("EN Error 1: " + countInformationWithMistake(ASCIIEn.Length, 1));
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number1 = new System.Windows.Forms.TextBox();
+            this.number2 = new System.Windows.Forms.TextBox();
+            this.number3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // number1
+            // 
+            this.number1.Location = new System.Drawing.Point(41, 39);
+            this.number1.Multiline = true;
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(111, 49);
+            this.number1.TabIndex = 0;
+            // 
+            // number2
+            // 
+            this.number2.Location = new System.Drawing.Point(192, 40);
+            this.number2.Multiline = true;
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(112, 48);
+            this.number2.TabIndex = 1;
+            // 
+            // number3
+            // 
+            this.number3.Location = new System.Drawing.Point(344, 40);
+            this.number3.Multiline = true;
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(112, 48);
+            this.number3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "НОД";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(192, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 64);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Поиск простых чисел";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(41, 224);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(415, 203);
+            this.result.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(344, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 64);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Число простое?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.number1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox number1;
+        private System.Windows.Forms.TextBox number2;
+        private System.Windows.Forms.TextBox number3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
