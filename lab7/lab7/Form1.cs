501a738abac51a1503f67f3fa5a8d119294cecbe
Julia <juliyanevar@gmail.com>
2021-04-14 10:38:17 +0300

    add an interface for lab7

---
+++ lab7/lab7/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+        DES_EEE2CryptoEncode encode = new DES_EEE2CryptoEncode();
+        DES_EEE2CryptoDecode decode = new DES_EEE2CryptoDecode();
+        string crypted, uncrypted;
+
+        public Form1()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crypted = encode.Encode(textBox1.Text, textBox3.Text, textBox4.Text);
+            textBox2.Text = crypted;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            uncrypted = decode.Decode(crypted, textBox3.Text, textBox4.Text);
+            textBox2.Text = uncrypted;
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
