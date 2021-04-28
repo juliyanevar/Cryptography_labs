e614889450e90d3aa67a52d81604a47f0f949d1b
Julia <juliyanevar@gmail.com>
2021-04-28 09:26:54 +0300

    fix lab 8

--- lab8.RC4/lab8.RC4/Form1.cs
+++ lab8.RC4/lab8.RC4/Form1.cs
@@ -14,43 +14,38 @@ namespace lab8.RC4
     {
         byte[] key = new byte[] { 61, 60, 23, 22, 21, 20 };
 
-
-        string Crypt(string data, byte[] key)
+        string Crypt(string data1, byte[] key)
         {
-            return Encoding.GetEncoding(1251).GetString(Crypt(Encoding.GetEncoding(1251).GetBytes(data), key));
-        }
-
-        byte[] Crypt(byte[] data, byte[] key)
-        {
+            byte[] data = Encoding.GetEncoding(1251).GetBytes(data1);
             int a, i, j, k, tmp;
-            int[] box = new int[256];
+            int[] S = new int[256];
             byte[] result = new byte[data.Length];
 
-            for (i = 0; i < box.Length; i++)
+            for (i = 0; i < S.Length; i++)
             {
-                box[i] = i;
+                S[i] = i;
             }
 
-            for (j = i = 0; i < box.Length; i++)
+            for (j = i = 0; i < S.Length; i++)
             {
-                j = (j + box[i] + key[i % key.Length]) % box.Length;
-                tmp = box[i];
-                box[i] = box[j];
-                box[j] = tmp;
+                j = (j + S[i] + key[i % key.Length]) % S.Length;
+                tmp = S[i];
+                S[i] = S[j];
+                S[j] = tmp;
             }
 
             for (a = j = i = 0; i < data.Length; i++)
             {
-                a = (a + 1) % box.Length;
-                j = (j + box[a]) % box.Length;
-                tmp = box[a];
-                box[a] = box[j];
-                box[j] = tmp;
-                k = box[(box[a] + box[j]) % box.Length];
+                a = (a + 1) % S.Length;
+                j = (j + S[a]) % S.Length;
+                tmp = S[a];
+                S[a] = S[j];
+                S[j] = tmp;
+                k = S[(S[a] + S[j]) % S.Length];
                 result[i] = (byte)(data[i] ^ k);
             }
 
-            return result;
+            return Encoding.GetEncoding(1251).GetString(result);
         }
 
         public Form1()
