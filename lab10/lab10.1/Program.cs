3d2a7aac85c370bddd831f2c44a2b65d49f2b058
Julia <juliyanevar@gmail.com>
2021-05-06 22:49:33 +0300

    start lab10

---
+++ lab10/lab10.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace lab10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            //DateTime t;
+            var timer = new Stopwatch();
+            var a = new BigInteger(13);
+            var n = new BigInteger(Encoding.UTF8.GetBytes("13131139780205884014566051738979"));
+            var x = new BigInteger(10);
+            var xArray = new BigInteger[]
+            {
+                BigInteger.Pow(x, 3),
+                BigInteger.Pow(x, 11),
+                BigInteger.Pow(x, 27),
+                BigInteger.Pow(x, 43),
+                BigInteger.Pow(x, 56)
+            };
+
+            foreach (var num in xArray)
+            {
+                //t = DateTime.Now;
+                //var y = BigInteger.ModPow(a, num, n);
+                //Console.WriteLine($"y = {y}, a = {a}, n = {n}, x = {num},\n time = {DateTime.Now-t}");
+                timer.Start();
+                var y = BigInteger.ModPow(a, num, n);
+                timer.Stop();
+                Console.WriteLine($"y = {y}, a = {a}, n = {n}, x = {num},\n time = {timer.Elapsed}");
+
+                timer.Reset();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
