5215b7587ce6c8fcfeea56f83d8d87d857bc2535
Julia <juliyanevar@gmail.com>
2021-05-18 19:35:13 +0300

    lab11

---
+++ lab11/lab11/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace lab11
+{
+    public partial class Form1 : Form
+    {
+        static Stopwatch timer = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetHash(string input)
+        {
+            timer.Reset();
+            timer.Start();
+            var md5 = MD5.Create();
+            var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+            timer.Stop();
+
+            return Convert.ToBase64String(hash);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = GetHash(textBox1.Text);
+            textBox3.Text = timer.Elapsed.ToString();
+        }
+    }
+}
